8ecd483c86e2ab233b775061f5d360be856190cf
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2023-08-31 10:22:53 +0700

    fix email to save

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+
+    has_many :microposts
+    #before_save { self.email = email.downcase! } # here has a bug when input a lowcase value ??
+    validates :name, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+
+    has_secure_password
+    #validates :password, presence: true, length: { minimum: 6 }
+end
